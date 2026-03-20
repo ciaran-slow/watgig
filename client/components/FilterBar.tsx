d1701d24bfb53933e5cb40f88c2c417571d626d7
Ciaran Slow <ciaranslow@hotmail.com>
2026-03-20 14:48:33 +1300

    updated filterbar

--- client/components/FilterBar.tsx
+++ client/components/FilterBar.tsx
@@ -10,77 +10,80 @@ function FilterBar({ filter, setFilter }: Props) {
     }`
 
   return (
-    <div className="p-12 flex flex-wrap gap-2">
-      <button 
-        onClick={() => setFilter('featured')}
-        className={getButtonClass('featured')}
-      >
-        Featured Events
-      </button>
+    <>
+      <h2>Filters</h2>
+      <div className="p-12 flex flex-wrap gap-2">
+        <button 
+          onClick={() => setFilter('featured')}
+          className={getButtonClass('featured')}
+        >
+          Featured Events
+        </button>
 
-      <button 
-        onClick={() => setFilter('week')}
-        className={getButtonClass('week')}
-      >
-        This Week
-      </button>
+        <button 
+          onClick={() => setFilter('week')}
+          className={getButtonClass('week')}
+        >
+          This Week
+        </button>
 
-      <button 
-        onClick={() => setFilter('month')}
-        className={getButtonClass('month')}
-      >
-        This Month
-      </button>
+        <button 
+          onClick={() => setFilter('month')}
+          className={getButtonClass('month')}
+        >
+          This Month
+        </button>
 
-      <button 
-        onClick={() => setFilter('rock')}
-        className={getButtonClass('rock')}
-      >
-        Rock / Indie
-      </button>
+        <button 
+          onClick={() => setFilter('rock')}
+          className={getButtonClass('rock')}
+        >
+          Rock / Indie
+        </button>
 
-      <button 
-        onClick={() => setFilter('pop')}
-        className={getButtonClass('pop')}
-      >
-        Pop
-      </button>
+        <button 
+          onClick={() => setFilter('pop')}
+          className={getButtonClass('pop')}
+        >
+          Pop
+        </button>
 
-      <button 
-        onClick={() => setFilter('electronic')}
-        className={getButtonClass('electronic')}
-      >
-        Electronic / DJ
-      </button>
+        <button 
+          onClick={() => setFilter('electronic')}
+          className={getButtonClass('electronic')}
+        >
+          Electronic / DJ
+        </button>
 
-      <button 
-        onClick={() => setFilter('hiphop')}
-        className={getButtonClass('hiphop')}
-      >
-        Hip-Hop / Rap
-      </button>
+        <button 
+          onClick={() => setFilter('hiphop')}
+          className={getButtonClass('hiphop')}
+        >
+          Hip-Hop / Rap
+        </button>
 
-      <button 
-        onClick={() => setFilter('acoustic')}
-        className={getButtonClass('acoustic')}
-      >
-        Acoustic
-      </button>
+        <button 
+          onClick={() => setFilter('acoustic')}
+          className={getButtonClass('acoustic')}
+        >
+          Acoustic
+        </button>
 
-      <button 
-        onClick={() => setFilter('jazz')}
-        className={getButtonClass('jazz')}
-      >
-        Jazz Blues
-      </button>
+        <button 
+          onClick={() => setFilter('jazz')}
+          className={getButtonClass('jazz')}
+        >
+          Jazz Blues
+        </button>
 
-      <button 
-        onClick={() => setFilter('metal')}
-        className={getButtonClass('metal')}
-      >
-        Metal / Punk
-      </button>  
-    </div>
+        <button 
+          onClick={() => setFilter('metal')}
+          className={getButtonClass('metal')}
+        >
+          Metal / Punk
+        </button>  
+      </div>
+    </>
   )
 }
 
