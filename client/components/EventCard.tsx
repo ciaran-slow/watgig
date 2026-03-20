9cd2881583c6818a9f32a93693188490eaf9a649
Ciaran Slow <ciaranslow@hotmail.com>
2026-03-20 14:43:28 +1300

    fixed filter bar alignment

--- client/components/EventCard.tsx
+++ client/components/EventCard.tsx
@@ -5,13 +5,14 @@ function EventCard() {
       style={{backgroundImage: "url('https://picsum.photos/400/400')", backgroundSize: "cover", backgroundPosition: 'center'}}
     >
       <div className="absolute top-6 right-6 bg-purple-500 p-2 rounded-xl">
-        <p>Featured</p>
+        <p>Featured Event</p>
       </div>
-      <div className="absolute bottom-6 left-6 p-4 rounded-xl bg-black">
+      <div className="absolute bottom-6 left-6 p-4 rounded-xl bg-black border-2 border-purple-500">
         <h2 className="text-xl font-bold">March 20</h2>
         <h2 className="text-4xl font-bold">Event Name</h2>
         <h3 className="text-xl font-bold">Featuring:</h3>
         <h3>Artist1, artist2, artist3 artist4</h3>
+        <h3 className="font-bold">Venue</h3>
       </div>
     </div>
   )
