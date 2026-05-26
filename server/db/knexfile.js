4632c59c627c5752d495055e7ab3d2dd852865db
Ciaran Slow <ciaranslow@hotmail.com>
2026-05-26 19:35:22 +1200

    update config

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -35,14 +35,19 @@ export default {
 
   production: {
     client: 'postgresql',
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT || 5432,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      ssl: { rejectUnauthorized: false }
-    },
+    connection: process.env.DATABASE_URL
+      ? {
+          connectionString: process.env.DATABASE_URL,
+          ssl: { rejectUnauthorized: false },
+        }
+      : {
+          host: process.env.DB_HOST,
+          port: process.env.DB_PORT || 5432,
+          database: process.env.DB_NAME,
+          user: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          ssl: { rejectUnauthorized: false },
+        },
     pool: {
       min: 2,
       max: 10,
